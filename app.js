1040a8e3f9745e29baf4dcdb32f0388d456ee3ae
Wuyoucen <youcen666@gamil.com>
2024-06-04 21:42:17 +0800

    router init

--- app.js
+++ app.js
@@ -1,13 +1,49 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
 const port = 3000;
 
+// 根目錄
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-app.get('/', (req,res)=>{
-  res.send('hello world')
+// 顯示餐廳全部清單頁面
+app.get("/restaurants", (req, res) => {
+  res.send("Welcome to the restaurant list");
+});
+
+// 顯示單一餐廳的頁面
+app.get("/restaurants/:id", (req, res) => {
+  res.send("Welcome to the restaurant - detail");
+});
+
+// 顯示新增餐廳的頁面(不做修改)
+app.get("/restaurants/new", (req, res) => {
+  res.send("This is the page for adding new restaurants");
+});
+
+// 新增餐廳(會修改，使用 POST)
+app.post("/restaurants/", (req, res) => {
+  res.send("add restaurant");
+});
+
+// 顯示編輯餐廳頁面(不做修改)
+app.get("/restaurants/:id/edit", (req, res) => {
+  res.send("This is the page for editing your restaurant");
+});
+
+// 更新、編輯餐廳(會修改，使用 PUT)
+app.put('/restaurant/:id',(req,res)=>{
+  res.send('update!!')
 })
 
-app.listen(port, ()=>{
+// 刪除餐廳(會修改)
+app.delete('restaurant/:id',(req,res)=>{
+  res.send('delete')
+})
+
+
+app.listen(port, () => {
   console.log(`server is running at http://localhost:${port} `);
-})
+});
